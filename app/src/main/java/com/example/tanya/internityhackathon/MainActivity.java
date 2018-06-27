d18fc1e184e7dee3c2425ff133cb5c28f4b00060
TAANYA <id.tanyagupta@gmail.com>
2018-06-27 15:46:11 +0530

    Search Bar Implemented

--- app/src/main/java/com/example/tanya/internityhackathon/MainActivity.java
+++ app/src/main/java/com/example/tanya/internityhackathon/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.tanya.internityhackathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +15,33 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        MenuItem item = menu.findItem(R.id.actionsearch);
+
+        SearchView searchView = (SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+        {
+            @Override
+            public boolean onQueryTextSubmit(String s)
+            {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s)
+            {
+
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
